8b8f0eec3f57f2c1994b1cc4e3e7cd79544e9324
Alexander Lyttle <a.j.lyttle@outlook.com>
2021-04-01 14:48:09 +0100

    Complete P5: Animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,9 +6,12 @@ const FRICT = 500
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer  # Variable created when Player node is ready
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -16,6 +19,9 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
+		# Animation
+		animationPlayer.play("RunRight")
+		
 		# Move at a constant speed
 #		velocity = input_vector / sqrt(pow(input_vector.x, 2) + pow(input_vector.y, 2))
 #		velocity = SPEED * input_vector.normalized()
@@ -23,6 +29,9 @@ func _physics_process(delta):
 		# Move with acceleration
 		velocity = velocity.move_toward(SPEED * input_vector.normalized(), ACCEL * delta)
 	else:
+		# Animation
+		animationPlayer.play("IdleRight")
+		
 		velocity = velocity.move_toward(Vector2.ZERO, FRICT * delta)  # Decelerate to zero
 	
 	velocity = move_and_slide(velocity)
