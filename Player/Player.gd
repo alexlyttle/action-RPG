d6acb8c9e9ed61fd093fb942021340358bc73b04
Alexander Lyttle <a.j.lyttle@outlook.com>
2021-04-01 15:17:38 +0100

    Complete P6: Animation Tree

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,8 @@ const FRICT = 500
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer  # Variable created when Player node is ready
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,20 +19,25 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
 		# Animation
-		animationPlayer.play("RunRight")
+		# Update blend position only when moving to set animation direction
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		# Update animation state
+		animationState.travel("Run")
 		
 		# Move at a constant speed
 #		velocity = input_vector / sqrt(pow(input_vector.x, 2) + pow(input_vector.y, 2))
 #		velocity = SPEED * input_vector.normalized()
 
 		# Move with acceleration
-		velocity = velocity.move_toward(SPEED * input_vector.normalized(), ACCEL * delta)
+		velocity = velocity.move_toward(SPEED * input_vector, ACCEL * delta)
 	else:
 		# Animation
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")  # Reset state to idle
 		
 		velocity = velocity.move_toward(Vector2.ZERO, FRICT * delta)  # Decelerate to zero
 	
