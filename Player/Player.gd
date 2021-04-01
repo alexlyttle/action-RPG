7346bdef6ce36a553a1d355ea68bc032d78835e1
Alexander Lyttle <a.j.lyttle@outlook.com>
2021-04-01 21:00:51 +0100

    Add alternative animation controlled by mouse position

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,32 +13,42 @@ onready var animationState = animationTree.get("parameters/playback")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
+	
+#	# Animation (alternative): player looks towards mouse position
+#	var mouse_vector = get_local_mouse_position()
+#	animationTree.set("parameters/Idle/blend_position", mouse_vector)
 	
 	if input_vector != Vector2.ZERO:
-		# Animation
+		# Normalise the input vector
+		input_vector = input_vector.normalized()
+		
+		# Animation: player faces direction of travel
 		# Update blend position only when moving to set animation direction
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		# Update animation state
 		animationState.travel("Run")
+
+#		# Animation (alternative): player looks towards mouse position
+#		animationTree.set("parameters/Run/blend_position", mouse_vector)
+#		# Update animation state
+#		animationState.travel("Run")
 		
 		# Move at a constant speed
 #		velocity = input_vector / sqrt(pow(input_vector.x, 2) + pow(input_vector.y, 2))
 #		velocity = SPEED * input_vector.normalized()
 
-		# Move with acceleration
+		# Move towards maximum speed with acceleration
 		velocity = velocity.move_toward(SPEED * input_vector, ACCEL * delta)
 	else:
-		# Animation
 		animationState.travel("Idle")  # Reset state to idle
 		
-		velocity = velocity.move_toward(Vector2.ZERO, FRICT * delta)  # Decelerate to zero
+		# Decelerate to zero given some friction
+		velocity = velocity.move_toward(Vector2.ZERO, FRICT * delta)
 	
 	velocity = move_and_slide(velocity)
