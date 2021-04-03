671afa8a3e33108da7103ff138f8cf838e7e248d
Alexander Lyttle <a.j.lyttle@outlook.com>
2021-04-04 00:04:17 +0100

    Changed Bat process to physics process

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -30,7 +30,7 @@ func seek_player():
 		state = CHASE
 
 
-func _process(delta):
+func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
 	knockback = move_and_slide(knockback)
 
